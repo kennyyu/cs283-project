ed4e22c5bcfcdd1db0af4a178ef4d5b545806202
Kenny Yu <kennyyu@college.harvard.edu>
2012-12-08 01:51:39 -0500

    made server threadsafe

--- server.py
+++ server.py
@@ -2,26 +2,41 @@ import base64
 import cv2
 import optical
 import os
+import threading
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
 
-DIRECTORY = "tmp"
-
-settings = {
-    "static_path": os.path.join(os.path.dirname(__file__), "static"),
-}
+# Directory for temporary files
+DIRECTORY = os.path.join(os.path.dirname(__file__), "tmp")
 
 class MainHandler(tornado.web.RequestHandler):
+    """
+    Handles the request for the default index.html web page.
+    """
     def get(self):
         self.render("index.html")
 
 class VideoWebSocketHandler(tornado.websocket.WebSocketHandler):
+    """
+    Creates a web socket connection to the client for receiving frames
+    and sending back the annotated frame and overall motion of the scene.
+    """
+
+    # Create a unique id per websocket
+    id = 0
+    lock = threading.Lock()
+
     def open(self):
-        print "Websocket opened"
+        with self.__class__.lock:
+            self.__class__.id += 1
+            self.id = self.__class__.id
+        self.file_in = DIRECTORY + "/in" + str(self.id) + ".jpeg"
+        self.file_out = DIRECTORY + "/out" + str(self.id) + ".png"
         self.prev = None
         if not os.path.exists(DIRECTORY):
             os.makedirs(DIRECTORY)
+        print "Websocket " + str(self.id) + " opened"
 
     def on_message(self, message):
         newim = self.parse_image(message)
@@ -29,27 +44,32 @@ class VideoWebSocketHandler(tornado.websocket.WebSocketHandler):
             self.prev = newim
         else:
             direction, retim = optical.detect_and_display(self.prev, newim)
-            cv2.imwrite(DIRECTORY + "/out.png", retim)
+            cv2.imwrite(self.file_out, retim)
             self.prev = newim
-            file = open(DIRECTORY + "/out.png", "r")
+            file = open(self.file_out, "r")
             self.write_message(direction + str(file.read()), binary=True)
             file.close()
 
     def on_close(self):
-        print "Websocket closed"
+        os.remove(self.file_in)
+        os.remove(self.file_out)
+        print "Websocket " + str(self.id) + " closed"
 
     def parse_image(self, buf):
         img_str = base64.b64decode(buf)
-        img = open(DIRECTORY + "/in.jpeg", "w+");
+        img = open(self.file_in, "w+");
         img.write(base64.b64decode(buf))
         img.close()
-        return cv2.imread(DIRECTORY + "/in.jpeg")
+        return cv2.imread(self.file_in)
+
+settings = {
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+}
 
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/websocket", VideoWebSocketHandler),
-    (r"/static", tornado.web.StaticFileHandler,
-     dict(path=settings['static_path'])),
+    (r"/static", tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
 ], **settings)
 
 if __name__ == "__main__":
